62761ed8fd29d24ca504ad8c1a0b8fb8bc305148
onkarj-98 <ojalimnche.17@gmail.com>
2020-05-19 15:50:30 +0530

    some new problems

---
+++ stack/min_max_stack.py
@@ -0,0 +1,28 @@
+class MinMax:
+    def __init__(self):
+        self.minMaxStack = []
+        self.stack = []
+
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
+
+    def pop(self):
+        self.minMaxStack.pop()
+        return self.stack.pop()
+
+    def push(self, number):
+        newMinMax = {"min": number, "max": number}
+        if len(self.minMaxStack):
+            lastminMax = self.minMaxStack[len(self.minMaxStack) - 1]
+            newMinMax["min"] = min(lastminMax, number)
+            newMinMax["max"] = max(lastminMax, number)
+
+        self.minMaxStack.append(newMinMax)
+        self.stack.append(number)
+
+    def getMin(self):
+        return self.minMaxStack[len(self.minMaxStack) - 1]["min"]
+
+    def getMax(self):
+        return self.minMaxStack[len(self.minMaxStack)- 1]["max"]
+
